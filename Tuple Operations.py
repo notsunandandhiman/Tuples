ce3303a0c511ff0626e5eb69ec1bc5a3826e2220
notsunandandhiman <150802724+notsunandandhiman@users.noreply.github.com>
2024-11-05 21:32:32 +0530

    Add files via upload

---
+++ Tuple Operations.py
@@ -0,0 +1,14 @@
+tuplex1= ("tuple", False, 3.532, 2)
+print(tuplex1)
+
+tuplex = (2, 4, 65, 7, 1, 24, 6, 5, 7)
+print(len(tuplex))
+
+tuple1= (50, 40, 4812, "Guddu", 73, "Guddu")
+print(tuple1.count(50))
+
+tuplex1= (2, 67, "don", "wqewq", 24, 5)
+slice1 = tuplex[4:5]
+
+print(slice1)
+print(tuplex[3])
